35dfbbf138d836295799aa9d3e65ec20b4250c1b
RaFaELO3 <rafaelmehdiyev0@gmail.com>
2023-02-01 17:20:15 +0400

    Profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -15,3 +15,30 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const tabs = document.querySelectorAll(".settings");
+const tab = document.querySelectorAll(".setting-tab");
+const panel = document.querySelectorAll(".panel");
+
+function onTabClick(event) {
+
+  for (let i = 0; i < tab.length; i++) {
+    tab[i].classList.remove("active");
+  }
+
+  for (let i = 0; i < panel.length; i++) {
+    panel[i].classList.remove("active");
+  }
+
+  event.target.classList.add('active');
+  let classString = event.target.getAttribute('data-target');
+  console.log(classString);
+  document.getElementById('profilePanel').getElementsByClassName(classString)[0].classList.add("active");
+}
+
+for (let i = 0; i < tab.length; i++) {
+  tab[i].addEventListener('click', onTabClick, false);
+}
+
+
+
