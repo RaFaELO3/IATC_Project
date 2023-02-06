2f476602d59519685476d44f432c2dd9d93bf0f1
RaFaELO3 <rafaelmehdiyev0@gmail.com>
2023-02-06 14:04:11 +0400

    Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,41 +20,9 @@ setInterval(function () {
 
 const foods = document.querySelectorAll('.food-box');
 const cats = document.querySelectorAll('.cat');
-const popup = document.querySelector('.food .popup');
-const popup1 = document.querySelector('.menu .popup');
-const close = document.querySelector('.close');
-const close1 = document.querySelector('.menu .close');
-const modalText = document.querySelector('.popup .modal-text');
 
-foods.forEach(food => {
-  food.addEventListener('click', function () {
-    open_close_modal();
-    document.querySelector('.popup img').src = food.children[0].getAttribute('src');
-    document.querySelector('.popup .name').innerText = food.children[1].children[0].innerText;
-    document.querySelector('.popup .price').innerText = food.children[1].children[1].innerText;
+cats.forEach(c=>{
+  c.addEventListener("click",()=>{
+    window.location.href = "menu-detail.html"
   })
 })
-
-cats.forEach(cat => {
-  cat.addEventListener('click', function () {
-    open_close_modal1();
-    document.querySelector('.popup .menu-img').src = cat.children[3].getAttribute('src');
-  })
-})
-
-close.addEventListener('click', function () {
-  open_close_modal();
-})
-
-close1.addEventListener('click', function () {
-  open_close_modal1();
-})
-
-function open_close_modal() {
-  popup.classList.toggle('active');
-  modalText.classList.toggle('active');
-}
-
-function open_close_modal1() {
-    popup1.classList.toggle('active');
-}
